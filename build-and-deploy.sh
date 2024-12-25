6ad75c71833cf782ed397d5380ed2dd880f877e1
huynd-dev <66739206+huynd-dev@users.noreply.github.com>
2024-12-25 11:46:55 +0700

    Update build-and-deploy.sh

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -3,16 +3,16 @@ TAG=${RANDOM:8:9}
 SERVICE=$1
 
 cd $SERVICE
-echo day la service $SERVICE
-# mvn package -DskipTests
-# sleep 30s
 
-# docker build -t dannhanong/$SERVICE:$TAG .
-# sleep 30s
+mvn package -DskipTests
+sleep 30s
 
-# docker push dannhanong/$SERVICE:$TAG
-# sleep 30s
+docker build -t dannhanong/$SERVICE:$TAG .
+sleep 30s
 
-# kubectl set image deployment/$SERVICE $SERVICE=dannhanong/$SERVICE:$TAG
+docker push dannhanong/$SERVICE:$TAG
+sleep 30s
 
-# echo "Deploy $SERVICE success!"
+kubectl set image deployment/$SERVICE $SERVICE=dannhanong/$SERVICE:$TAG
+
+echo "Deploy $SERVICE success!"
